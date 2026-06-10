adc0e79ea7fed4a3baca5905ca61f908900d0408
1822319085-png <1822319085@qq.com>
2026-06-10 20:44:29 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
+# -*- coding: utf-8 -*-
+
 import streamlit as st
+import joblib
+import os
+
 from lifecycle_page import render_lifecycle_app
 from direct_page import render_direct_prediction_app
+
 
 st.set_page_config(
     page_title="Seismic Failure Mode Probability Assessment",
@@ -8,10 +14,23 @@ st.set_page_config(
     initial_sidebar_state="collapsed"
 )
 
+
+@st.cache_resource
+def load_numpy_assets():
+    assets_path = "model_assets_numpy.pkl"
+    if not os.path.exists(assets_path):
+        return None
+    return joblib.load(assets_path)
+
+
+assets = load_numpy_assets()
+
+
 if "page_mode" not in st.session_state:
     st.session_state.page_mode = "lifecycle"
 
+
 if st.session_state.page_mode == "lifecycle":
-    render_lifecycle_app()
+    render_lifecycle_app(assets=assets)
 else:
-    render_direct_prediction_app()
+    render_direct_prediction_app(assets=assets, show_back_button=True)
